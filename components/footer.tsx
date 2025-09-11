1014a6133d458dc3df3398ea7ab6818ab89ae394
Anelka MD <anelkabagalaneofficiel@gmail.com>
2025-09-11 12:28:21 +0200

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -28,7 +28,7 @@ const links = [
     },
 ]
 
-update export default function FooterSection() {
+export default function FooterSection() {
     return (
         <footer className="py-16 md:py-32">
             <div className="mx-auto max-w-5xl px-6">
